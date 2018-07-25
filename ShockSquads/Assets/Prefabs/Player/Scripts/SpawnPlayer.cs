24fbf97f7f21f6028b396711e6279108d84c18b7
jdmajernik <jdmajernik@gmail.com>
2018-07-24 21:48:13 -0400

    added in multiplayer room, subsequently broke it

--- ShockSquads/Assets/Prefabs/Player/Scripts/SpawnPlayer.cs
+++ ShockSquads/Assets/Prefabs/Player/Scripts/SpawnPlayer.cs
@@ -1,16 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class SpawnPlayer : MonoBehaviour {
+public class SpawnPlayer : NetworkBehaviour {
+    //Written by John Majernik
+    //This script sets up the player between the local (first person) and the server (third person) prefabs
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //public variables
+    public enum playerLoadout { laser };
+
+    //private variables
+    [SerializeField] private GameObject localPrefabLaser;
+    [SerializeField] private GameObject serverPrefabLaser;
+
+    public override void OnStartLocalPlayer()
+    {
+        
+    }
+    private void Start()
+    {
+        if(isLocalPlayer)
+        {
+            Instantiate(localPrefabLaser, transform);
+        }
+        else
+        {
+            Instantiate(serverPrefabLaser, transform);
+        }
+    }
 }
