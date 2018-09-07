2d980065ad5d0ba41f7beca0fafbd7eb0e08f3a0
jdmajernik <jdmajernik@gmail.com>
2018-09-06 21:22:06 -0400

    new branch, suckas!

--- ShockSquads/Assets/Networking/MatchMaking/Scripts/MatchLobbyManager.cs
+++ ShockSquads/Assets/Networking/MatchMaking/Scripts/MatchLobbyManager.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mono.Nat;
 
 public class MatchLobbyManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        NatUtility.StartDiscovery();
+        INatDevice device = 
 	}
 	
 	// Update is called once per frame
